fe13a708e4552cb6650668ce649c1ac238875cf8
Valentine <valoderah@gmail.com>
2019-02-21 21:51:03 -0600

    Added ajax

--- script/script_contact.js
+++ script/script_contact.js
@@ -1,12 +1,15 @@
+
+ //   const ans = document.getElementById("answerSqr");
+
 function myfunction() {
     let side = document.getElementById("calculate").value;
     let area = parseInt(side) * parseInt(side);
-    alert("The area of a square is: "+ area);
+    document.getElementById("answerSqr").innerHTML = 'Answer: ' + area;
 }
 
 function myrectangle() {
     let hnum = document.getElementById("height").value;
     let wnum = document.getElementById("weight").value;
     let arearec = parseInt(hnum) * parseInt(wnum);
-    alert("The area of the rectangle is: "+arearec);
+    document.getElementById("answerrec").innerHTML = 'Answer: ' + arearec;
 }
