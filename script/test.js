2491a53e0d1a13a9b3647d820b963cb07c2ee7a0
Valentine <valoderah@gmail.com>
2019-02-21 23:10:37 -0600

    Final rehearsal

--- script/test.js
+++ script/test.js
@@ -1,6 +1,13 @@
 QUnit.test('Testing calculateArea function with several sets of inputs', function (assert) {
-    assert.equal(myFunction(3),9, 'Tested with two relatively small positive numbers');
-    assert.equal(myFunction(0),0, 'Tested with side value 0. ');
-    assert.equal(myFunction(896785667456),0,'Tested with hours<0, result will be 0. ' );
-    assert.equal(myFunction(-9),0, 'Tested with side value 0. ');
+    assert.equal(myFunction(7),49, 'Tested with int');
+    assert.equal(myFunction(0),0, 'Tested to see if 0. ');
+    assert.equal(myFunction(6000),0,'Tried to keep to the scope of my testing. ' );
+    assert.equal(myFunction(-5),0, 'Value 0 if negative. ');
 });
+
+// QUnit.test('Testing calculateArea function with several sets of inputs', function (assert) {
+//     assert.equal(rec(2,5),10, 'Tested with ');
+//     assert.equal(rec(0),0, 'Tested to see if 0. ');
+//     assert.equal(rec(5000,1),0,'Tried to keep to the scope of my testing. ' );
+//     assert.equal(rec(-5,4),0, 'Value 0 if negative. ');
+// });
--- script/val.js
+++ script/val.js
@@ -13,3 +13,20 @@ function myFunction(area) {
     }
     return num;
 }
+
+
+// function rec(weight, height) {
+//     value1 = weight;
+//     value2 = height
+//     var n;
+//     if (val<0){
+//         n = 0
+//     }
+//     else if (val>2500){
+//         n=0
+//     }
+//     else{
+//         n = weight * height;
+//     }
+//     return n;
+// }
